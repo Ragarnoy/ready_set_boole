2ec9d549241c8afbb44a0ec34172af9c07e213fa
Tiago Lernould <tiagolernould@gmail.com>
2022-05-27 21:30:18 +0200

    wip not binary expr to nnf and unit tests + refactor not unitary

--- boolean_evaluation/src/node/nnf.rs
+++ boolean_evaluation/src/node/nnf.rs
@@ -1,34 +1,33 @@
 use crate::node::Node;
 use crate::operator::Operator;
 
-pub fn not_expr_to_nnf(node: &mut Node) -> Node {
-    match node {
-        Node::UnaryExpr { op, child } => not_expr_to_nnf(child),
-        Node::BinaryExpr { op, lhs, rhs } => match *op {
+pub fn not_binary_expr_to_nnf(node: Node) -> Node {
+    if let Node::BinaryExpr { op, lhs, rhs } = node {
+        match op {
             Operator::Imply => Node::BinaryExpr {
                 op: Operator::Or,
                 lhs: Box::new(Node::UnaryExpr {
                     op: Operator::Not,
-                    child: Box::new(not_expr_to_nnf(lhs)),
+                    child: lhs,
                 }),
-                rhs: Box::new(not_expr_to_nnf(rhs)),
+                rhs,
             },
 
             Operator::Xnor => Node::BinaryExpr {
                 op: Operator::Xor,
-                lhs: Box::new(not_expr_to_nnf(lhs)),
-                rhs: Box::new(not_expr_to_nnf(rhs)),
+                lhs,
+                rhs,
             },
 
             Operator::And => Node::BinaryExpr {
                 op: Operator::Or,
                 lhs: Box::new(Node::UnaryExpr {
                     op: Operator::Not,
-                    child: Box::new(not_expr_to_nnf(lhs)),
+                    child: lhs,
                 }),
                 rhs: Box::new(Node::UnaryExpr {
                     op: Operator::Not,
-                    child: Box::new(not_expr_to_nnf(rhs)),
+                    child: rhs,
                 }),
             },
 
@@ -36,11 +35,11 @@ pub fn not_expr_to_nnf(node: &mut Node) -> Node {
                 op: Operator::And,
                 lhs: Box::new(Node::UnaryExpr {
                     op: Operator::Not,
-                    child: Box::new(not_expr_to_nnf(lhs)),
+                    child: lhs,
                 }),
                 rhs: Box::new(Node::UnaryExpr {
                     op: Operator::Not,
-                    child: Box::new(not_expr_to_nnf(rhs)),
+                    child: rhs,
                 }),
             },
 
@@ -50,22 +49,68 @@ pub fn not_expr_to_nnf(node: &mut Node) -> Node {
                     op: Operator::And,
                     lhs: Box::new(Node::UnaryExpr {
                         op: Operator::Not,
-                        child: Box::new(not_expr_to_nnf(lhs)),
+                        child: lhs.clone(),
                     }),
-                    rhs: Box::new(not_expr_to_nnf(rhs)),
+                    rhs: rhs.clone(),
                 }),
                 rhs: Box::new(Node::BinaryExpr {
                     op: Operator::And,
-                    lhs: Box::new(not_expr_to_nnf(lhs)),
+                    lhs,
                     rhs: Box::new(Node::UnaryExpr {
                         op: Operator::Not,
-                        child: Box::new(not_expr_to_nnf(rhs)),
+                        child: rhs,
                     }),
                 }),
             },
-            _ => {
-                unreachable!()
+            Operator::Not => {
+                unreachable!();
             }
+        }
+    } else {
+        node
+    }
+}
+
+pub fn not_unary_expr_to_nnf(node: Node) -> Node {
+    match node {
+        Node::UnaryExpr { child, .. } => match *child {
+            Node::UnaryExpr { child, .. } => *child,
+            _ => not_unary_expr_to_nnf(*child),
         },
+        _ => node,
+    }
+}
+
+#[cfg(test)]
+mod nnf_test {
+    use crate::node::nnf::{not_binary_expr_to_nnf, not_unary_expr_to_nnf};
+    use crate::node::Node;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_not_unary_odd() {
+        let three = Node::from_str("1!!!").unwrap();
+        assert_eq!(not_unary_expr_to_nnf(three), Node::from_str("1!").unwrap());
+        let five = Node::from_str("1!!!!!").unwrap();
+        assert_eq!(not_unary_expr_to_nnf(five), Node::from_str("1!").unwrap());
+    }
+
+    #[test]
+    fn test_not_unary_even() {
+        let two = Node::from_str("1!!").unwrap();
+        assert_eq!(not_unary_expr_to_nnf(two), Node::from_str("1").unwrap());
+        let four = Node::from_str("1!!!!").unwrap();
+        assert_eq!(not_unary_expr_to_nnf(four), Node::from_str("1").unwrap());
+    }
+
+    #[test]
+    fn test_not_binary_expr() {
+        let and: Node = Node::from_str("11&").unwrap();
+        let not_or = Node::from_str("11|!").unwrap();
+        let not_xor = Node::from_str("11^!").unwrap();
+        assert_eq!(
+            not_binary_expr_to_nnf(and),
+            Node::from_str("1!1!|").unwrap()
+        );
     }
 }
