d15613a1209e97655d4b87fa36ffd7c147ebec46
Tiago Lernould <tiagolernould@gmail.com>
2022-05-30 22:42:42 +0200

    debug for operator now prints normal characters because why not

--- boolean_evaluation/src/operator.rs
+++ boolean_evaluation/src/operator.rs
@@ -1,7 +1,7 @@
 use std::fmt;
-use std::fmt::{Display, Formatter};
+use std::fmt::{Debug, Display, Formatter};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 pub enum Operator {
     Not,
     And,
@@ -21,6 +21,19 @@ impl Display for Operator {
             Operator::Xor => write!(f, "⊕"),
             Operator::Imply => write!(f, "→"),
             Operator::Xnor => write!(f, "⇔"),
+        }
+    }
+}
+
+impl Debug for Operator {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Operator::Not => write!(f, "!"),
+            Operator::And => write!(f, "&"),
+            Operator::Or => write!(f, "|"),
+            Operator::Xor => write!(f, "^"),
+            Operator::Imply => write!(f, ">"),
+            Operator::Xnor => write!(f, "="),
         }
     }
 }
