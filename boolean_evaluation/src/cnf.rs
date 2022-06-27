8bce1903d6ddaff56011ad966c7091d73d2c038f
Tiago Lernould <tiagolernould@gmail.com>
2022-06-27 21:52:10 +0200

    distribute both but wrong variable order

--- boolean_evaluation/src/cnf.rs
+++ boolean_evaluation/src/cnf.rs
@@ -3,18 +3,57 @@ use crate::node::Node;
 use crate::operator::Operator;
 
 pub fn node_to_cnf(node: Node) -> Node {
-    if let Node::BinaryExpr { op, lhs, rhs } = node_to_negation_normal_form(node.clone()) {
+    let node = node_to_negation_normal_form(node);
+    if let Node::BinaryExpr { op, lhs, rhs } = node {
         match op {
             Operator::And => Node::BinaryExpr {
                 op: Operator::And,
                 lhs: Box::new(node_to_cnf(*lhs)),
                 rhs: Box::new(node_to_cnf(*rhs)),
             },
-            Operator::Or => Node::BinaryExpr {
-                op: Operator::And,
-                lhs: Box::new(node_to_cnf(*lhs)),
-                rhs: Box::new(node_to_cnf(*rhs)),
-            },
+            Operator::Or => {
+                let has_and = (
+                    matches!(
+                        *lhs,
+                        Node::BinaryExpr {
+                            op: Operator::And,
+                            ..
+                        }
+                    ),
+                    matches!(
+                        *rhs,
+                        Node::BinaryExpr {
+                            op: Operator::And,
+                            ..
+                        }
+                    ),
+                );
+                if has_and == (true, true) {
+                    distribute_both(Node::BinaryExpr {
+                        op: Operator::Or,
+                        lhs: Box::new(node_to_cnf(*lhs)),
+                        rhs: Box::new(node_to_cnf(*rhs)),
+                    })
+                } else if has_and == (true, false) {
+                    distribute_left(Node::BinaryExpr {
+                        op: Operator::Or,
+                        lhs: Box::new(node_to_cnf(*lhs)),
+                        rhs: Box::new(node_to_cnf(*rhs)),
+                    })
+                } else if has_and == (false, true) {
+                    distribute_right(Node::BinaryExpr {
+                        op: Operator::Or,
+                        lhs: Box::new(node_to_cnf(*lhs)),
+                        rhs: Box::new(node_to_cnf(*rhs)),
+                    })
+                } else {
+                    Node::BinaryExpr {
+                        op: Operator::Or,
+                        lhs: Box::new(node_to_cnf(*lhs)),
+                        rhs: Box::new(node_to_cnf(*rhs)),
+                    }
+                }
+            }
             _ => unreachable!(),
         }
     } else {
@@ -23,6 +62,75 @@ pub fn node_to_cnf(node: Node) -> Node {
 }
 
 // Distribute conjunctions over disjunctions.
-fn distribute(factor: Node, node: Node) -> Node {
+fn distribute_both(node: Node) -> Node {
+    if let Node::BinaryExpr { op: Operator::Or, lhs, rhs } = node {
+        match (*rhs, *lhs) {
+            (
+                Node::BinaryExpr {
+                    op: Operator::And,
+                    lhs: lhs_lhs,
+                    rhs: lhs_rhs,
+                },
+                Node::BinaryExpr {
+                    op: Operator::And,
+                    lhs: rhs_lhs,
+                    rhs: rhs_rhs,
+                },
+            ) => {
+                return Node::BinaryExpr {
+                    op: Operator::And,
+                    lhs: Box::new(Node::BinaryExpr {
+                        op: Operator::And,
+                        lhs: Box::new(node_to_cnf(Node::BinaryExpr {
+                            op: Operator::Or,
+                            lhs: Box::new(*lhs_lhs.clone()),
+                            rhs: Box::new(*rhs_lhs.clone()),
+                        })),
+                        rhs: Box::new(node_to_cnf(Node::BinaryExpr {
+                            op: Operator::Or,
+                            lhs: Box::new(*lhs_lhs),
+                            rhs: Box::new(*rhs_rhs.clone()),
+                        })),
+                    }),
+                    rhs: Box::new(Node::BinaryExpr {
+                        op: Operator::And,
+                        lhs: Box::new(node_to_cnf(Node::BinaryExpr {
+                            op: Operator::Or,
+                            lhs: Box::new(*lhs_rhs.clone()),
+                            rhs: Box::new(*rhs_lhs),
+                        })),
+                        rhs: Box::new(node_to_cnf(Node::BinaryExpr {
+                            op: Operator::Or,
+                            lhs: Box::new(*lhs_rhs),
+                            rhs: Box::new(*rhs_rhs),
+                        })),
+                    }),
+                }
+            },
+            _ => unreachable!(),
+        }
+        }
+        todo!()
+    }
+
+fn distribute_left(node: Node) -> Node {
     todo!()
+}
+
+fn distribute_right(node: Node) -> Node {
+    todo!()
+}
+
+#[cfg(test)]
+mod cnf_test {
+    use std::str::FromStr;
+    use crate::cnf::node_to_cnf;
+    use crate::node::Node;
+
+    #[test]
+    fn distribute_both() {
+        let node = Node::from_str("AB&CD&|").unwrap();
+        let result = Node::from_str("AC|AD|&BC|BD|&&").unwrap();
+        assert_eq!(node_to_cnf(node), result);
+    }
 }
