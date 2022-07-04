47cdd6327533a1c2632c4073ced22149ad248730
Tiago Lernould <tiagolernould@gmail.com>
2022-07-04 15:19:41 +0200

    ex06

--- ex06/src/main.rs
+++ ex06/src/main.rs
@@ -1,3 +1,17 @@
+use boolean_evaluation::cnf::node_to_cnf;
+use boolean_evaluation::node::Node;
+use std::str::FromStr;
+
+fn conjunctive_normal_form(formula: &str) -> String {
+    let node = Node::from_str(formula).unwrap();
+    node_to_cnf(node).print_rpn()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 && !args[1].is_empty() {
+        conjunctive_normal_form(&args[1]);
+    } else {
+        conjunctive_normal_form("AB&C|");
+    }
 }
