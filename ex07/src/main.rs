ee66ad2737ecce4c1e30c567931b67cfd4ae4840
Tiago Lernould <tiagolernould@gmail.com>
2022-07-08 19:53:38 +0200

    ex07 template

---
+++ ex07/src/main.rs
@@ -0,0 +1,16 @@
+use std::str::FromStr;
+use boolean_evaluation::tree::Tree;
+
+fn sat(formula: &str) -> bool {
+    let node = Tree::from_str(formula).unwrap();
+    node.sat()
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 && !args[1].is_empty() {
+        println!("{}", sat(&args[1]));
+    } else {
+        println!("{}", sat("10|"));
+    }
+}
