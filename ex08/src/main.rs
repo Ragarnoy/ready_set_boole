b0fcf53d3d89c398b9de8775ca552eb4e3ed9c59
Tiago Lernould <tiagolernould@gmail.com>
2022-07-15 19:45:20 +0200

    powerset wip

--- ex08/src/main.rs
+++ ex08/src/main.rs
@@ -1,3 +1,17 @@
+fn powerset(set: &[i32]) -> Vec<Vec<i32>> {
+    (0..2usize.pow(set.len() as u32))
+        .map(|i| {
+            set.iter()
+                .enumerate()
+                .filter(|&(t, _)| (i >> t) % 2 == 1)
+                .map(|(_, element)| *element)
+                .collect()
+        })
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let res = powerset(&[1, 0, 3]);
+    dbg!(0..2usize.pow(3));
+    println!("{:?}", res);
 }
