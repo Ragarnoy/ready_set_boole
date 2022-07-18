7ee49c50ac97d3df37eded8d9949c99d81cf9503
Tiago Lernould <tiagolernould@gmail.com>
2022-07-18 21:52:33 +0200

    unneeded argument default value is always false, new member

--- boolean_evaluation/src/variable.rs
+++ boolean_evaluation/src/variable.rs
@@ -2,11 +2,15 @@
 pub struct Variable {
     pub name: char,
     pub value: bool,
-    set: Option<Vec<i32>>,
+    pub set: Option<Vec<i32>>,
 }
 
 impl Variable {
     pub fn new(name: char) -> Self {
-        Self { name, value: false, set: None }
+        Self {
+            name,
+            value: false,
+            set: None,
+        }
     }
 }
