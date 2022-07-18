d434efee92fb9508e4745b57612c3f366d74fec3
Tiago Lernould <tiagolernould@gmail.com>
2022-07-18 21:51:16 +0200

    rustfmt

--- ex08/src/main.rs
+++ ex08/src/main.rs
@@ -13,7 +13,10 @@ fn powerset(set: &[i32]) -> Vec<Vec<i32>> {
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 && !args[1].is_empty() {
-        let set: Vec<i32> = args[1].split(',').map(|x| x.parse::<i32>().unwrap()).collect();
+        let set: Vec<i32> = args[1]
+            .split(',')
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
         println!("{:?}", powerset(&set));
     } else {
         println!("{:?}", powerset(&[1, 2, 3]));
