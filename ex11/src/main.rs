eecca7f6baea07c22707f65ec687e744571906fc
Tiago Lernould <tiagolernould@gmail.com>
2022-07-25 20:30:46 +0200

    getting there

--- ex11/src/main.rs
+++ ex11/src/main.rs
@@ -1,13 +1,17 @@
 const ORDER: usize = 2_usize.pow(32);
 
+fn hilbert_curve(n: u32) -> (u16, u16) {
+   todo!()
+}
+
 fn reverse_map(n: f64) -> (u16, u16) {
-    let mut new_n = n;
-    for i in 1..ORDER {
-        new_n = n >> 2;
+    if !(0.0..=1.0).contains(&n) {
+        panic!("n must be between 0.0 and 1.0");
     }
-    todo!()
+    hilbert_curve((n * (2.0f64.powf(32.0) - 1.0)) as u32)
 }
 
 fn main() {
-    println!("Hello, world!");
+    let result = reverse_map(0.5);
+    println!("{} {}", result.0, result.1);
 }
