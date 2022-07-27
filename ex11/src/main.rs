c1af472deb2638a4352e1067f16d18f02384a181
Tiago Lernould <tiagolernould@gmail.com>
2022-07-27 19:26:55 +0200

    getting somewhere fast

--- ex11/src/main.rs
+++ ex11/src/main.rs
@@ -1,7 +1,37 @@
-const ORDER: usize = 2_usize.pow(32);
+use std::mem;
 
-fn hilbert_curve(n: u32) -> (u16, u16) {
-   todo!()
+const ORDER: usize = 2_usize.pow(16);
+
+fn hilbert_curve(mut n: u32) -> (u16, u16) {
+    let mut order = ORDER as u32;
+    let positions = [(0, 0), (1, 0), (1, 1), (0, 1)];
+
+    let (mut x, mut y): (u32, u32) = positions[n as usize & 3];
+    n >>= 2;
+    while order > 0 {
+        order >>= 1;
+
+        match n & 3 {
+            0 => {
+                mem::swap(&mut x, &mut y);
+            }
+            1 => {
+                y += order;
+            }
+            2 => {
+                x += order;
+                y += order;
+            }
+            3 => {
+                let tmp = y;
+                y = order.wrapping_sub(1).wrapping_sub(x);
+                x = order.wrapping_sub(1).wrapping_sub(tmp);
+            }
+            _ => {}
+        }
+    }
+
+    (x as u16, y as u16)
 }
 
 fn reverse_map(n: f64) -> (u16, u16) {
@@ -12,6 +42,12 @@ fn reverse_map(n: f64) -> (u16, u16) {
 }
 
 fn main() {
-    let result = reverse_map(0.5);
-    println!("{} {}", result.0, result.1);
+    let mut f = 0.0;
+    while f < 1.0 {
+        let (x, y) = reverse_map(f);
+        println!("{} => {} {}", f, x, y);
+        f += 0.01;
+    }
+    // let result = reverse_map(0.5);
+    // println!("{} {}", result.0, result.1);
 }
