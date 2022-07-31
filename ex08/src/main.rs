0b74d8c555fcfd22dbd9527dfcf78083b689664b
Tiago Lernould <tiagolernould@gmail.com>
2022-07-31 19:32:26 +0200

    add unit tests

--- ex08/src/main.rs
+++ ex08/src/main.rs
@@ -3,7 +3,7 @@ fn powerset(set: &[i32]) -> Vec<Vec<i32>> {
         .map(|i| {
             set.iter()
                 .enumerate()
-                .filter(|&(t, _)| (i >> t) % 2 == 1)
+                .filter(|&(t, _)| (i >> t) & 1 == 1)
                 .map(|(_, element)| *element)
                 .collect()
         })
@@ -20,5 +20,44 @@ fn main() {
         println!("{:?}", powerset(&set));
     } else {
         println!("{:?}", powerset(&[1, 2, 3]));
+    }
+}
+
+#[cfg(test)]
+mod test_powerset {
+    use super::powerset;
+
+    #[test]
+    fn test_none() {
+        assert_eq!(powerset(&[]), vec![vec![]]);
+    }
+
+    #[test]
+    fn test_one() {
+        assert_eq!(powerset(&[1]), vec![vec![], vec![1]]);
+    }
+
+    #[test]
+    fn test_two() {
+        assert_eq!(powerset(&[1, 2]), vec![
+            vec![],
+            vec![1],
+            vec![2],
+            vec![1, 2],
+        ]);
+    }
+
+    #[test]
+    fn test_three() {
+        assert_eq!(powerset(&[1, 2, 3]), vec![
+            vec![],
+            vec![1],
+            vec![2],
+            vec![1, 2],
+            vec![3],
+            vec![1, 3],
+            vec![2, 3],
+            vec![1, 2, 3],
+        ]);
     }
 }
